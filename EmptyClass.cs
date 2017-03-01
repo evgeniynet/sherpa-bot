eb37bdfc1b05ca57019e0afb3c25f82b21b2ac21
Eugene <jtrue@mail.ru>
2017-03-01 14:01:35 +0200

    added logs

---
+++ EmptyClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Application
+{
+	public class EmptyClass
+	{
+		public EmptyClass ()
+		{
+			string test;
+			int rr;
+			rr.ToString ();
+			foreach (var post in posts) {
+			}
+			for (int i = 0; i < 5; i++) {
+			}
+		}
+	}
+}
+
